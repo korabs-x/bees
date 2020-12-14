f492ad4a2d6a15334319e9c8ae10351fefc247e9
Simon Boehm <simon@siboehm.com>
2020-12-14 16:17:15 +0100

    Add new script for Azure

--- scripts/azure/train_obj_detection_azure.sh
+++ scripts/azure/train_obj_detection_azure.sh
@@ -90,9 +90,6 @@ printf "bee\n" > obj.names
 printf 'classes = 2\ntrain = %s/train.txt\nvalid = %s/test.txt\nnames = obj.names\n' "$HOME" "$HOME" > obj.data
 printf 'backup = %s/backup-%s\n' "$HOME" "$PREFIX" >> obj.data
 
-# start background backup process
-screen -d -m -S backup_process bash "$HOME/periodic_backup.sh" "$PREFIX"
-
 # run training
 ./darknet detector train obj.data yolov4-custom.cfg $weightsfile -dont_show -map
 cd
@@ -114,5 +111,3 @@ if [[ ! -f "$scorefile" ]]; then
     done
 fi
 $GSUTIL cp "$scorefile" "$GSBUCKET"
-
-sudo shutdown -h
