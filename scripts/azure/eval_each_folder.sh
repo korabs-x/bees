46de34b743e136b0e7166a062e58918ce122562e
Simon Boehm <simon@siboehm.com>
2020-12-17 08:30:44 +0100

    Add script to evaluate on each data folder

---
+++ scripts/azure/eval_each_folder.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -xeuo pipefail
+
+if (( $# != 2)); then
+    echo "run using $0 <PREFIX> <WEIGHTSFILE>"
+    exit 1
+fi
+
+PREFIX=$1
+WEIGHTSFILE=$2
+
+cd "$HOME/darknet" || exit
+printf "bee\n" > obj.names
+printf 'classes = 2\ntrain = %s/train.txt\nvalid = %s/test.txt\nnames = obj.names\n' "$HOME/darknet" "$HOME/darknet" > obj.data
+printf 'backup = %s/backup-%s\n' "$HOME" "$PREFIX" >> obj.data
+scorefile="scores-${PREFIX}.txt"
+for hive in $(find "$HOME/data" -mindepth 1 -maxdepth 1 -type d); do
+    find "$hive" -type f | grep -E "(jpg|jpeg|png)" | grep -E "(/validate/|/test/)" | xargs realpath > "test.txt"
+    printf '\n\nHIVE: %s\n' "$hive" >> "$scorefile"
+    ./darknet detector map obj.data yolov4-custom.cfg "$HOME/$WEIGHTSFILE" -thresh 0.25 >> "$scorefile"
+done
