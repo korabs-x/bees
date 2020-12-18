32e619008896d6534d8c04cde217c2b3fcf42423
Simon Boehm <simon@siboehm.com>
2020-12-18 08:10:24 +0100

    Small changes

--- scripts/azure/eval_each_folder.sh
+++ scripts/azure/eval_each_folder.sh
@@ -14,10 +14,10 @@ cd "$HOME/darknet" || exit
 printf "bee\n" > obj.names
 printf 'classes = 2\ntrain = %s/train.txt\nvalid = %s/test.txt\nnames = obj.names\n' "$HOME/darknet" "$HOME/darknet" > obj.data
 printf 'backup = %s/backup-%s\n' "$HOME" "$PREFIX" >> obj.data
-scorefile="scores-${PREFIX}.txt"
-echo -n "" > "$scorefile"
+scorefile="scores-each-${PREFIX}.txt"
 for hive in $(find "$HOME/data" -mindepth 1 -maxdepth 1 -type d); do
     find "$hive" -type f | grep -E "(jpg|jpeg|png)" | grep -E "(/validate/|/test/)" | xargs realpath > "test.txt"
     printf '\n\nHIVE: %s\n' "$hive" | tee --append "$scorefile"
     ./darknet detector map obj.data yolov4-custom.cfg "$HOME/$WEIGHTSFILE" -thresh 0.25 >> "$scorefile"
 done
+cd "$HOME"
